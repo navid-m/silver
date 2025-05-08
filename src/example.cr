7e3b24b92a120cd795a393a1a1a9c7cfa931bc1e
Navid M <navidmomtahen@gmail.com>
2025-05-08 22:04:55 +0100

    amend example

--- src/example.cr
+++ src/example.cr
@@ -4,7 +4,7 @@ port = 8082
 router = Silver::Router.new
 
 router.add_route("/") do |ctx|
-    res = HttpResponse.new
+    res = Silver::HttpResponse.new
     body = "<h1>Hello from Silver Web Framework</h1>"
     res.mime = "text/html"
     res.data = body.to_slice
