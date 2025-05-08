18d21cf6831954d24fe16e6c9ff7ad97d1e9e845
Navid M <navidmomtahen@gmail.com>
2025-05-08 21:11:18 +0100

    update

--- src/example.cr
+++ src/example.cr
@@ -3,16 +3,16 @@ require "../src/silver"
 port = 8082
 router = Silver::Router.new
 
-# router.add_route("/") do |ctx|
-#     res = HttpResponse.new
-#     body = "<h1>Hello from Silver Web Framework</h1>"
-#     res.mime = "text/html"
-#     res.data = body.to_slice
-#     res.content_length = body.bytesize
-#     res
-# end
-
 router.add_route("/") do |ctx|
+    res = HttpResponse.new
+    body = "<h1>Hello from Silver Web Framework</h1>"
+    res.mime = "text/html"
+    res.data = body.to_slice
+    res.content_length = body.bytesize
+    res
+end
+
+router.add_route("/json") do |ctx|
     res = Silver::HttpResponse.new
     body = %({"message": "hello world"})
     res.mime = "application/json"
