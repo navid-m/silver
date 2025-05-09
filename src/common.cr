d30eb965266322585b88ebc8a7ca9316e544776a
Navid M <navidmomtahen@gmail.com>
2025-05-09 18:14:07 +0100

    amend

--- src/common.cr
+++ src/common.cr
@@ -1,8 +1,8 @@
 module Silver
-    DEFAULT_PORT         = 8082
-    ROOT                 = "./static/"
-    FLAG_PORT_HELP       = "the port to listen for requests"
-    CANNOT_OPEN_PORT_MSG = "cannot connect to specified port\n"
-    NOT_FOUND_MSG        = "404 Not Found\r\n"
-    BAD_REQ_MSG          = "400 Bad Request\r\n"
+    private DEFAULT_PORT         = 8082
+    private ROOT                 = "./static/"
+    private FLAG_PORT_HELP       = "the port to listen for requests"
+    private CANNOT_OPEN_PORT_MSG = "cannot connect to specified port\n"
+    private NOT_FOUND_MSG        = "404 Not Found\r\n"
+    private BAD_REQ_MSG          = "400 Bad Request\r\n"
 end
